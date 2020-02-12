7a3c970100070b19fb7bfb65301e4674f1477423
下地明文 <mifukia@mifukia.local>
2020-02-12 20:11:46 +0900

    controller 作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function __invoke($id = 'noname',$pass = 'unknown'){
+        return <<<EOF
+        <html>
+        <title>Hello/Index</title>
+        <style>
+        body{
+            font-size:16px;color:#999;
+        }
+        </style>
+        <body>
+            <h1>Index</h1>
+            <p>これは、Helloコントローラーのindexアクションです。</p>
+            <ul>
+                <li>ID:{$id}</li>
+                <li>PASS:{$pass}</li>
+            </ul>
+        </body>
+        </html>
+EOF;
+    }
 }
