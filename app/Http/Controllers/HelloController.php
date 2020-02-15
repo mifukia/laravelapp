3ab0c89faed0adb5f308fc8d2112aea436e26446
下地明文 <mifukia@mifukia.local>
2020-02-15 17:28:49 +0900

    update

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,27 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function __invoke($id = 'noname',$pass = 'unknown'){
-        return <<<EOF
-        <html>
-        <title>Hello/Index</title>
-        <style>
-        body{
-            font-size:16px;color:#999;
-        }
-        </style>
-        <body>
-            <h1>Index</h1>
-            <p>これは、Helloコントローラーのindexアクションです。</p>
-            <ul>
-                <li>ID:{$id}</li>
-                <li>PASS:{$pass}</li>
-            </ul>
-        </body>
-        </html>
-EOF;
+    public function index(Request $request){
+        $data = [
+            'msg' => 'これはコントローラーから渡されたメッセージです。',
+            'id' => $request -> id
+        ];
+        return view('hello.index',$data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{id?}/{pass?}', 'HelloController');
+Route::get('hello', 'HelloController@index');
