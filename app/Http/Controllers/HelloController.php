cef7224d265563af15e296008a104f9373eb63d1
下地明文 <mifukia@mifukia.local>
2020-02-21 18:52:03 +0900

    自前のバリデータを作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Http\Requests\HelloRequest;
+//use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -21,9 +22,35 @@ class HelloController extends Controller
     //     ]);
     // }
     public function index(Request $request){
-        return view('hello.index',['msg' => 'フォームを入力: ']);
+        $validator = Validator::make($request -> query(),[
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if($validator -> fails()){
+            $msg = 'クエリーに問題があります。';
+        }else{
+            $msg = 'ID/PASSを受付けました。フォームを入力ください。';
+        };
+        return view('hello.index',['msg' => $msg]);
     }
-    public function post(HelloRequest $request){
+    public function post(Request $request){
+        $rules = [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ];
+        $messages = [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入ください。',
+            'age.between' => '年齢は０〜１５０の間で入力ください。'
+        ];
+        $validator = Validator::make($request -> all(),$rules,$messages);
+        if($validator -> fails()){
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
         return view('hello.index',['msg' => '正しく入力されました！']);
     }
 }
