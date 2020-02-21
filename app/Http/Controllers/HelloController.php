19b95ebe70c68c69a84f04f48aeaea9ad0ad6b1b
下地明文 <mifukia@mifukia.local>
2020-02-21 18:22:36 +0900

    フォームリクエストを利用してみる

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
@@ -22,13 +23,7 @@ class HelloController extends Controller
     public function index(Request $request){
         return view('hello.index',['msg' => 'フォームを入力: ']);
     }
-    public function post(Request $request){
-        $validate__rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150'
-        ];
-        $this -> validate($request,$validate__rule);
+    public function post(HelloRequest $request){
         return view('hello.index',['msg' => '正しく入力されました！']);
     }
 }
